aa7b6cca88bafe3fd289bfa9b776d8efe2b214f1
Alan Jhonnes <aj@alanjhonnes.com>
2014-08-25 16:28:24 -0300

    angular frontend

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('ccdiTv.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('PageContainerController', ['$scope', function($scope) {
+    var pages = [];
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('PageController', ['$scope', function($scope) {
 
   }]);
--- web/js/directives.js
+++ web/js/directives.js
@@ -9,9 +9,31 @@ angular.module('ccdiTv.directives', []).
       elm.text(version);
     };
   }]).
-  directive("pageContainer", [null, function(){
+  directive("pageContainer", function(){
+    return {
+        restrict: "E",
+        controller: function(){
+            this.pages = [];
+            this.currentPage = 0;
+            this.minDelay = 5000;
 
-    }])
+            this.registerPage = function(page){
+                this.pages.push(page);
+            }
+        }
+    }
+    }).
+    directive("page", function(){
+        return {
+            restrict: "E",
+            require: "^page-container",
+            link: function (scope, element, attrs, pageContainerCtrl) {
+                //pageContainerCtrl.registerPage();
+                console.log("page registered");
+            }
+
+        }
+    })
 
 
 ;
